e011a1f4c5fb3ebea91326d91ffd970bb6ba4992
tera-tools <none@none.com>
2020-02-06 13:47:00 -0500

    fixed settings version

--- settings.js
+++ settings.js
@@ -16,8 +16,10 @@ module.exports = function MigrateSettings(from_ver, to_ver, settings) {
     } else if (from_ver === null) {
         // No config file exists, use default settings
         return DefaultSettings;
-    } else {
+    } else {        
+        if (from_ver < to_ver)
+            return Object.assign(Object.assign({}, DefaultSettings), settings);
         // Migrate from older version (using the new system) to latest one
-        throw new Error('So far there is only one settings version and this should never be reached!');
+        throw new Error('Settings version is newer than previous this should never be reached!');
     }
 }
